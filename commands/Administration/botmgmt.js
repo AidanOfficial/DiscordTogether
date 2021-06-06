cb8683d23b5730f8830d5938ee509eb7b1bad43c
AidxnnRBLX <hi@aidan.pw>
2021-06-05 20:57:09 -0400

    Added levels and errorCodes to botmgmt, embed, and say

--- commands/Administration/botmgmt.js
+++ commands/Administration/botmgmt.js
@@ -5,6 +5,7 @@ const config = require("../../botconfig/config.json");
 const ee = require("../../botconfig/embed.json");
 const errorCodes = require("../../botconfig/errorCodes.json");
 const rgx = /^(?:<@!?)?(\d+)>?$/;
+const staff = require("../../botconfig/whitelist.json");
 
 module.exports = {
   name: "botmgmt",
@@ -12,60 +13,75 @@ module.exports = {
   aliases: ["mgmt", "admin", "shortcuts", "management", "botmanagement"],
   cooldown: 2,
   usage: "botmgmt <shortcut>",
-  description: "",
+  description: "Use DiscordTogether administration commands. Restricted to Bot Admins+.",
   run: async (client, message, args, user, text, prefix) => {
+    try {
+      if (!staff.allStaff.includes(message.author.id)) {
+        return message.channel.send(new MessageEmbed()
+          .setColor(ee.wrongcolor)
+          .setFooter(ee.footertext, ee.footericon)
+          .setTitle(`❌ ERROR: 100 | An error occurred`)
+          .setDescription(`${errorCodes.adminErrors[100]}`)
+        );
+      }
 
-    const whitelist = [ // Defines Bot Management Whitelist
-      "337325419416125442" // AidanOfficial#0001
-    ];
+      const choice = args[0]; // Defines the choice that the user made
+      const secondaryOption = args[1]; // Defines the secondary choice that the user made (optional)
 
-    if (!whitelist.includes(message.author.id)) { // Returns an error if user is not on the Bot Management Whitelist (const whitelist)
+      if (!choice) {
+        return message.channel.send(new MessageEmbed()
+          .setColor(ee.wrongcolor)
+          .setFooter(ee.footertext, ee.footericon)
+          .setTitle(`❌ ERROR: 201 | An error occurred`)
+          .setDescription(`${errorCodes.cmdErrors[201]}`)
+        );
+      }
+
+      if (choice.toLowerCase() == 'codelist' || choice.toLowerCase() == 'list' || choice.toLowerCase() == 'actions') { // Code List
+        const choiceListEmbed = new MessageEmbed()
+          .setColor(ee.color)
+          .setFooter(ee.footertext, ee.footericon)
+          .setTitle(`ℹ | DiscordTogether | Bot Management Code List`)
+          .setDescription(`\`1\` **| Leave Guild | Args: [1] GuildID**`)
+        message.member.send(choiceListEmbed);
+      }
+
+      if (choice.toLowerCase() == '1' || choice.toLowerCase() == 'leaveguild') { // Guild Leave
+        const guildID = secondaryOption; // Defines the GuildID
+        if (!rgx.test(guildID)) {
+          return message.channel.send(new MessageEmbed()
+            .setColor(ee.wrongcolor)
+            .setFooter(ee.footertext, ee.footericon)
+            .setTitle(`❌ ERROR: 101 | An error occurred`)
+            .setDescription(`${errorCodes.adminErrors[101]}`)
+          );
+        }
+        const guild = message.client.guilds.cache.get(guildID);
+        if (!guild) {
+          return message.channel.send(new MessageEmbed()
+            .setColor(ee.wrongcolor)
+            .setFooter(ee.footertext, ee.footericon)
+            .setTitle(`❌ ERROR: 102 | An error occurred`)
+            .setDescription(`${errorCodes.adminErrors[102]}`)
+          );
+        }
+        await guild.leave();
+        message.channel.send(new MessageEmbed()
+          .setColor(ee.color)
+          .setFooter(ee.footertext, ee.footericon)
+          .setTitle(`✅ SUCCESS | Guild Left`)
+          .setDescription(`The provided guild has been left.`)
+        )
+
+      }
+    } catch (e) {
+      console.log(String(e.stack).bgRed)
       return message.channel.send(new MessageEmbed()
         .setColor(ee.wrongcolor)
         .setFooter(ee.footertext, ee.footericon)
-        .setTitle(`❌ ERROR: 100 | An error occurred`)
-        .setDescription(`${errorCodes.adminErrors[100]}`)
+        .setTitle(`❌ ERROR | An error occurred`)
+        .setDescription(`\`\`\`${e.stack}\`\`\``)
       );
-    }
-
-    const choice = args[0]; // Defines the choice that the user made
-    const secondaryOption = args[1]; // Defines the secondary choice that the user made (optional)
-
-    if (choice.toLowerCase() == 'codelist' || choice.toLowerCase() == 'list' || choice.toLowerCase() == 'actions') { // Code List
-      const choiceListEmbed = new MessageEmbed()
-        .setColor(ee.color)
-        .setFooter(ee.footertext, ee.footericon)
-        .setTitle(`ℹ | DiscordTogether | Bot Management Code List`)
-        .setDescription(`\`1\` **| Leave Guild | Args: [1] GuildID**`)
-      message.member.send(choiceListEmbed);
-    }
-
-    if (choice.toLowerCase() == '1' || choice.toLowerCase() == 'leaveguild') { // Guild Leave
-      const guildID = secondaryOption; // Defines the GuildID
-      if (!rgx.test(guildID)) {
-        return message.channel.send(new MessageEmbed()
-        .setColor(ee.wrongcolor)
-        .setFooter(ee.footertext, ee.footericon)
-        .setTitle(`❌ ERROR: 101 | An error occurred`)
-        .setDescription(`${errorCodes.adminErrors[101]}`)
-      );}
-      const guild = message.client.guilds.cache.get(guildID);
-      if (!guild) {
-          return message.channel.send(new MessageEmbed()
-          .setColor(ee.wrongcolor)
-          .setFooter(ee.footertext, ee.footericon)
-          .setTitle(`❌ ERROR: 102 | An error occurred`)
-          .setDescription(`${errorCodes.adminErrors[102]}`)
-        );
-      }
-      await guild.leave();
-      message.channel.send(new MessageEmbed()
-        .setColor(ee.color)
-        .setFooter(ee.footertext, ee.footericon)
-        .setTitle(`✅ SUCCESS | Guild Left`)
-        .setDescription(`The provided guild has been left.`)
-      )
-      
     }
   }
 }
--- commands/Administration/say.js
+++ commands/Administration/say.js
@@ -1,31 +1,46 @@
-const { MessageEmbed } = require("discord.js");
+const {
+  MessageEmbed
+} = require("discord.js");
 const config = require("../../botconfig/config.json");
 const ee = require("../../botconfig/embed.json");
+const staff = require("../../botconfig/whitelist.json");
+const errorCodes = require("../../botconfig/errorCodes.json");
+
 module.exports = {
-    name: "say",
-    category: "Administration",
-    aliases: [""],
-    cooldown: 2,
-    usage: "say <TEXT>",
-    description: "Resends your Text",
-    run: async (client, message, args, user, text, prefix) => {
-    try{
-      if(!args[0])
+  name: "say",
+  category: "Administration",
+  aliases: [""],
+  cooldown: 2,
+  usage: "say <TEXT>",
+  description: "Resends your Text",
+  run: async (client, message, args, user, text, prefix) => {
+    try {
+
+      if (!staff.allStaff.includes(message.author.id)) {
         return message.channel.send(new MessageEmbed()
-            .setColor(ee.wrongcolor)
-            .setFooter(ee.footertext, ee.footericon)
-            .setTitle(`❌ ERROR | You didn't provided a Text`)
-            .setDescription(`Usage: \`${prefix}${this.usage}\``)
+          .setColor(ee.wrongcolor)
+          .setFooter(ee.footertext, ee.footericon)
+          .setTitle(`❌ ERROR: 100 | An error occurred`)
+          .setDescription(`${errorCodes.adminErrors[100]}`)
+        );
+      }
+
+      if (!args[0])
+        return message.channel.send(new MessageEmbed()
+          .setColor(ee.wrongcolor)
+          .setFooter(ee.footertext, ee.footericon)
+          .setTitle(`❌ ERROR | You didn't provided a Text`)
+          .setDescription(`Usage: \`${prefix}${this.usage}\``)
         );
       message.channel.send(text);
     } catch (e) {
-        console.log(String(e.stack).bgRed)
-        return message.channel.send(new MessageEmbed()
-            .setColor(ee.wrongcolor)
-            .setFooter(ee.footertext, ee.footericon)
-            .setTitle(`❌ ERROR | An error occurred`)
-            .setDescription(`\`\`\`${e.stack}\`\`\``)
-        );
+      console.log(String(e.stack).bgRed)
+      return message.channel.send(new MessageEmbed()
+        .setColor(ee.wrongcolor)
+        .setFooter(ee.footertext, ee.footericon)
+        .setTitle(`❌ ERROR | An error occurred`)
+        .setDescription(`\`\`\`${e.stack}\`\`\``)
+      );
     }
   }
 }
